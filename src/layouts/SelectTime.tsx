6adb8af390b340b37a68980773c241e30bb3062a
Piotr <Piotr@DESKTOP-OGB5VIU>
2023-03-18 12:21:31 +0100

    Github changes

--- src/layouts/SelectTime.tsx
+++ src/layouts/SelectTime.tsx
@@ -4,6 +4,16 @@ interface SelectTime {
     onTimeSelect: (selectedTime: string) => void;
   }
 
+  useEffect(() => {
+    fetch('http://localhost:8081/workers')
+      .then(response => response.json())
+      .then((data: Worker[]) => {
+        //setOptions(data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+
 const SelectTime = ({ onTimeSelect }: SelectTime) => {
     const timeOptions = ['9:00', '9:15', '9:30', '9:45', '10:00'];
   
